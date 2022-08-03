518cfcad1172a45e26678c809327507f2c014dda
cert <2715084160@qq.com>
2022-08-03 13:41:13 +0800

    dev batch first commit

---
+++ src/day20/InsertSort01.java
@@ -0,0 +1,27 @@
+package day20;
+
+import java.util.Arrays;
+
+/**
+ * ≤Â»Î≈≈–Ú¡∑œ∞
+ */
+public class InsertSort01 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{32, 23, 12, 11, 0, -23};
+        insertSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void insertSort(int[] arr) {
+        int length = arr.length;
+        for (int i = 1; i < length; i++) {
+            int j = i - 1;
+            int value = arr[i];
+            while (j >= 0 && arr[j] > value) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = value;
+        }
+    }
+}
